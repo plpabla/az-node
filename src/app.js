482a35c9cc5200b7b9767c8952eb474023122d22
pawel-laptop <p.blaszczy@gmail.com>
2023-12-12 19:23:52 +0100

    test of the full pipeline

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const http = require('node:http');
-// Note: for Node we must use 0.0.0.0
+// Note: for Node we must use 0.0.0.0 if we want to run it in a container
 // https://medium.com/@marcmintel/development-setup-with-nuxt-node-and-docker-b008a241c11d#fb37
 const host = '0.0.0.0';
 const port = process.env.PORT || 6900;
@@ -8,7 +8,7 @@ const server = http.createServer((req,res)=>{
     // res.writeHead(200, {'Content-Type': 'text/plain'});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello, world (from Node container, yay!)\nAzure container, yay! #PARTY\nCD enabled (webhook working but only after manual reset)\nMaybe now will be better?\n");
+    res.end("Hello, world (from Node, Container, Azure Container Registry, Github, and Webhooks for CD");
 });
 
 server.listen(port, host, () => {
